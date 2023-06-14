2ac2c130654e051ee9259267b5c13de692d4f8de
Ayodeji Komolafe <kvngeko@icloud.com>
2023-06-14 05:17:31 -0400

    Initial Commit

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Kvng Eko on 6/14/23.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
