9459a74e3fc2b0f375e593ee4adcc165fe311497
Ayodeji Komolafe <kvngeko@icloud.com>
2023-06-14 07:24:01 -0400

    location list view started

--- DubDubGrub/LocationListView.swift
+++ DubDubGrub/LocationListView.swift
@@ -9,12 +9,52 @@ import SwiftUI
 
 struct LocationListView: View {
     var body: some View {
-        Text("Location List")
+        NavigationView {
+            List {
+                ForEach(0..<10) { item in
+                    HStack {
+                        Image("default-square-asset")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 80, height: 80)
+                            .clipShape(Circle())
+                            .padding(.vertical, 8)
+                        
+                        VStack(alignment: .leading) {
+                            Text("Test Location Name")
+                                .font(.title2)
+                                .fontWeight(.semibold)
+                                .lineLimit(1)
+                                .minimumScaleFactor(0.75)
+                            HStack {
+                                AvatarView()
+                                AvatarView()
+                                AvatarView()
+                                AvatarView()
+                                AvatarView()
+                            }
+                        }
+                        .padding(.leading)
+                    }
+                }
+            }
+            .navigationTitle("Grub Spots")
+        }
     }
 }
 
 struct LocationListView_Previews: PreviewProvider {
     static var previews: some View {
         LocationListView()
+    }
+}
+
+struct AvatarView: View {
+    var body: some View {
+        Image("default-avatar")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 35, height: 45)
+            .clipShape(Circle())
     }
 }
