1ea41a9dd14aff0ff56bb02b00abf3efc32ad6e1
Ayodeji Komolafe <kvngeko@icloud.com>
2023-06-14 08:34:40 -0400

    location detail view started

---
+++ DubDubGrub/LocationDetailView.swift
@@ -0,0 +1,22 @@
+//
+//  LocationDetailView.swift
+//  DubDubGrub
+//
+//  Created by Kvng Eko on 6/14/23.
+//
+
+import SwiftUI
+
+struct LocationDetailView: View {
+    var body: some View {
+        NavigationView {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+    }
+}
+
+struct LocationDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationDetailView()
+    }
+}
