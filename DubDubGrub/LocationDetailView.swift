ec55f5353f623e23df41d3c731b1aa87dab1f9eb
Ayodeji Komolafe <kvngeko@icloud.com>
2023-07-04 06:45:41 -0400

    LocationDetailView started

--- DubDubGrub/LocationDetailView.swift
+++ DubDubGrub/LocationDetailView.swift
@@ -10,7 +10,14 @@ import SwiftUI
 struct LocationDetailView: View {
     var body: some View {
         NavigationView {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            VStack{
+                Image("default-banner-asset")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: 120)
+                    Spacer()
+            }
+            .navigationTitle("Location Name")
         }
     }
 }
