c8aca5ab8d3352918053dc869b0c7be1b54c67e4
Ayodeji Komolafe <kvngeko@icloud.com>
2023-07-04 07:09:23 -0400

    LoadingDetailView Cont'd

--- DubDubGrub/LocationDetailView.swift
+++ DubDubGrub/LocationDetailView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct LocationDetailView: View {
     var body: some View {
         NavigationView {
-            VStack(spacing:16){
+            VStack(spacing: 16){
                 Image("default-banner-asset")
                     .resizable()
                     .scaledToFill()
@@ -30,6 +30,29 @@ struct LocationDetailView: View {
                     Capsule()
                         .frame(height: 80)
                         .foregroundColor(Color(.secondarySystemBackground))
+                    HStack(spacing: 20) {
+                        Button {
+                            
+                        } label: {
+                            LocationActionButton(color: .brandPrimary, imageName: "location.fill")
+                        }
+                        
+                        Link(destination: URL(string: "https://www.apple.com")!, label: {
+                            LocationActionButton(color: .brandPrimary, imageName: "network")
+                        })
+                             
+                        Button {
+                            
+                        } label: {
+                            LocationActionButton(color: .brandPrimary, imageName: "phone.fill")
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            LocationActionButton(color: .brandPrimary, imageName: "person.fill.checkmark")
+                        }
+                    }
                 }
                 .padding(.horizontal)
                 
@@ -44,5 +67,23 @@ struct LocationDetailView: View {
 struct LocationDetailView_Previews: PreviewProvider {
     static var previews: some View {
         LocationDetailView()
+    }
+}
+
+struct LocationActionButton: View {
+    var color: Color
+    var imageName: String
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .foregroundColor(color)
+                .frame(width: 60, height: 60)
+            Image(systemName: imageName)
+                .resizable()
+                .scaledToFit()
+                .foregroundColor(.white)
+                .frame(width: 22, height: 22)
+        }
     }
 }
