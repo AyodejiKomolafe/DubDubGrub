2e0334b11209dd8f930e69fe55e4ef2f13191f58
Ayodeji Komolafe <kvngeko@icloud.com>
2023-07-04 09:41:46 -0400

    profile cont'd

--- DubDubGrub/ProfileView.swift
+++ DubDubGrub/ProfileView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var companyName = ""
+    @State private var bio = ""
     var body: some View {
         VStack {
             ZStack {
@@ -15,7 +19,7 @@ struct ProfileView: View {
                     .frame(height: 130)
                     .cornerRadius(12)
                     .padding(.horizontal)
-                HStack {
+                HStack(spacing: 16) {
                     ZStack {
                         AvatarView(size: 84)
                         Image(systemName: "square.and.pencil")
@@ -25,7 +29,21 @@ struct ProfileView: View {
                             .foregroundColor(.white)
                             .offset(y: 30)
                     }
+                    .padding(.leading, 12)
+                    VStack(spacing: 1) {
+                        TextField("First Name", text: $firstName)
+                            .font(.system(size: 32, weight: .bold))
+                            .lineLimit(1)
+                            .minimumScaleFactor(0.75)
+                        TextField("Last Name", text: $lastName)
+                            .font(.system(size: 32, weight: .bold))
+                            .lineLimit(1)
+                            .minimumScaleFactor(0.75)
+                        TextField("Company Name", text: $companyName)
+                    }
+                    .padding(.trailing, 16)
                 }
+                .padding()
             
             }
             
