be6ff392adf4c833c99d399d90b1d2b2936c1eed
Ayodeji Komolafe <kvngeko@icloud.com>
2023-07-04 09:30:58 -0400

    profile View started

--- DubDubGrub/ProfileView.swift
+++ DubDubGrub/ProfileView.swift
@@ -9,12 +9,36 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        Text("Profile")
+        VStack {
+            ZStack {
+                Color(.secondarySystemBackground)
+                    .frame(height: 130)
+                    .cornerRadius(12)
+                    .padding(.horizontal)
+                HStack {
+                    ZStack {
+                        AvatarView(size: 84)
+                        Image(systemName: "square.and.pencil")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 14, height: 14)
+                            .foregroundColor(.white)
+                            .offset(y: 30)
+                    }
+                }
+            
+            }
+            
+            Spacer()
+        }
+        .navigationTitle("Profile")
     }
 }
 
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
-        ProfileView()
+        NavigationView {
+            ProfileView()
+        }
     }
 }
