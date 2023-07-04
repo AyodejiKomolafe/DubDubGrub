1384970bda740524aba151d2d56d4a1da6840c61
Ayodeji Komolafe <kvngeko@icloud.com>
2023-07-04 06:55:20 -0400

    LodingDetailView Cont'd

--- DubDubGrub/LocationDetailView.swift
+++ DubDubGrub/LocationDetailView.swift
@@ -10,14 +10,33 @@ import SwiftUI
 struct LocationDetailView: View {
     var body: some View {
         NavigationView {
-            VStack{
+            VStack(spacing:16){
                 Image("default-banner-asset")
                     .resizable()
                     .scaledToFill()
                     .frame(height: 120)
+                HStack{
+                    Label("123 main st", systemImage: "mappin.and.ellipse")
+                        .font(.caption)
+                        .foregroundColor(.secondary)
                     Spacer()
+                }
+                .padding(.horizontal)
+                Text("This is a test Description. This is a test Description. This is a test Description.")
+                    .lineLimit(3)
+                    .minimumScaleFactor(0.75)
+                    .padding(.horizontal)
+                ZStack {
+                    Capsule()
+                        .frame(height: 80)
+                        .foregroundColor(Color(.secondarySystemBackground))
+                }
+                .padding(.horizontal)
+                
+                Spacer()
             }
             .navigationTitle("Location Name")
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
