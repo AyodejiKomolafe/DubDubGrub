e8fdf3ebe281d6edd07f3b50efd2d88a9aeecb94
Ayodeji Komolafe <kvngeko@icloud.com>
2023-07-26 19:36:48 -0400

    profile CloudKit model created

--- DubDubGrub/Model/DDGLocation.swift
+++ DubDubGrub/Model/DDGLocation.swift
@@ -5,8 +5,41 @@
 //  Created by Kvng Eko on 7/26/23.
 //
 
-import Foundation
+import CloudKit
 
 struct DDGLocation {
+    static let kName = "name"
+    static let kDescription = "description"
+    static let kSquareAsset = "squareAsset"
+    static let kBannerAsset = "bannerAsset"
+    static let kAddress = "address"
+    static let kLocation = "location"
+    static let kWebsiteURL = "websiteURL"
+    static let kPhoneNumber = "phoneNumber"
     
+    
+    
+    
+    let ckRecordID: CKRecord.ID
+    let name: String
+    let description: String
+    let squareAsset: CKAsset!
+    let bannerAsset: CKAsset!
+    let address: String
+    let location: CLLocation
+    let websiteURL: String
+    let phoneNumber: String
+    
+    init(record: CKRecord) {
+        ckRecordID = record.recordID
+        name = record[DDGLocation.kName] as? String ?? "N/A"
+        description = record[DDGLocation.kDescription] as? String ?? "N/A"
+        squareAsset = record[DDGLocation.kSquareAsset] as? CKAsset
+        bannerAsset = record[DDGLocation.kBannerAsset] as? CKAsset
+        address = record[DDGLocation.kAddress] as? String ?? "N/A"
+        location = record[DDGLocation.kLocation] as? CLLocation ?? CLLocation(latitude: 0, longitude: 0)
+        websiteURL = record[DDGLocation.kWebsiteURL] as? String ?? "N/A"
+        phoneNumber = record[DDGLocation.kPhoneNumber] as? String ?? "N/A"
+        
+    }
 }
